2f134c7a7c89538776e342302a4805acf26d9827
Hogyi1 <hogyor.peter@gmail.com>
2025-11-17 00:14:45 +0100

    Small changes to be a parent

--- Assets/_Scripts/Player/Interaction/PlayerController.cs
+++ Assets/_Scripts/Player/Interaction/PlayerController.cs
@@ -1,16 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IObjectParent<IHoldableItem>
 {
     [SerializeField] Interactor interactor;
     [SerializeField] InputHandler input;
+    [SerializeField] Transform handTransform;
 
     PlayerModel playerModel;
+    public bool hasChild;
+    [SerializeField] KitchenObject debugHeldItem;
 
     private void Awake()
     {
         interactor = GetComponentInChildren<Interactor>();
         playerModel = new PlayerModel();
+
+        if (debugHeldItem != null)
+        {
+            debugHeldItem.SetParent(this);
+        }
     }
 
     private void OnEnable() => input.Interact += OnInteractionPressed;
@@ -22,13 +31,44 @@ public class PlayerController : MonoBehaviour
         var interactable = interactor.GetInteractable();
         if (interactable == null) return;
 
-        var ir = interactable.CanInteract(playerModel);
-        if (!ir.success)
-        {
-            Debug.Log(ir.message); // Tell the UIManager or something
-            return;
-        }
-
-        interactable.OnInteract(playerModel); // Lets interact
+        var ir = interactable.TryInteract(this);
+        Debug.Log(ir.message);
     }
+
+    public Transform GetParentPosition() => handTransform;
+
+    public void SetChild(IHoldableItem child)
+    {
+        playerModel.Pickup(child);
+    }
+
+    public bool HasChild() => playerModel.HeldItem != null;
+
+    public KitchenObject TryGetKitchenObject()
+    {
+        if (playerModel.HeldItem is KitchenObject kitchenObject)
+            return kitchenObject;
+        return null;
+    }
+
+    public void ClearHeldItem() => playerModel.Pickup(null);
+
+    public IHoldableItem GetChild() => playerModel.HeldItem;
+
+    private void Update()
+    {
+        hasChild = HasChild();
+    }
+
+    private void OnValidate()
+    {
+        if (debugHeldItem != null)
+        {
+            SetChild(debugHeldItem);
+            debugHeldItem.SetParent(this);
+        }
+    }
+
+    public void SetChild(IObjectChild child) => SetChild((IHoldableItem)child);
+    IObjectChild IObjectParent.GetChild() => GetChild();
 }
