1273e86c7e1ab3e0772df5077650b160e89bffca
Hogyi1 <hogyor.peter@gmail.com>
2025-11-17 00:11:35 +0100

    Added visual classes

---
+++ Assets/_Scripts/Visuals/BaseVisual.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseVisual : MonoBehaviour, ISelectable
+{
+    public abstract void OnDeselect();
+
+    public abstract void OnSelect();
+
+    [SerializeField] protected GameObject selectedVisual;
+    protected float currentOpacity;
+    protected List<Material> allMaterials = new();
+
+    private void Start()
+    {
+        selectedVisual.SetActive(false);
+
+        Renderer[] renderers = selectedVisual.GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            var materials = renderer.materials;
+            allMaterials.AddRange(materials);
+        }
+
+        SetAllMaterialsAlpha(0f);
+        currentOpacity = 0f;
+    }
+
+    protected void SetAllMaterialsAlpha(float alpha)
+    {
+        foreach (var mat in allMaterials)
+        {
+            Color color = mat.color;
+            color.a = alpha;
+            mat.color = color;
+            currentOpacity = alpha;
+        }
+    }
+}
---
+++ Assets/_Scripts/Visuals/PulsatingVisual.cs
@@ -0,0 +1,62 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class PulsatingVisual : BaseVisual
+{
+    [SerializeField] private float pulsingTime = 1f;
+    [SerializeField] private float fadeInTime = 0.25f;
+    [SerializeField] private float fadeOutTime = 0.25f;
+    [SerializeField] private float lowerFadeAmount = 0.45f;
+    [SerializeField] private float upperFadeAmount = 0.85f;
+
+    Tween fadeTween;
+
+    private void Awake()
+    {
+        selectedVisual = selectedVisual != null ? selectedVisual : transform.Find("Visual").gameObject;
+    }
+
+    public override void OnSelect()
+    {
+        Debug.Log("Selected");
+        fadeTween.Kill();
+        selectedVisual.SetActive(true);
+
+        fadeTween = DOTween.To(() => currentOpacity,
+            x => SetAllMaterialsAlpha(x),
+            upperFadeAmount, fadeInTime)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() =>
+            {
+                fadeTween = DOTween.To(() => upperFadeAmount,
+                    x => SetAllMaterialsAlpha(x),
+                    lowerFadeAmount,
+                    pulsingTime)
+                    .SetEase(Ease.InOutSine)
+                    .SetLoops(-1, LoopType.Yoyo);
+            });
+    }
+
+    public override void OnDeselect()
+    {
+        Debug.Log("Deselected");
+        fadeTween.Kill();
+        fadeTween = DOTween.To(() => currentOpacity,
+            x => SetAllMaterialsAlpha(x),
+            0f,
+            fadeOutTime)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() =>
+            {
+                SetAllMaterialsAlpha(0f);
+                selectedVisual.SetActive(false);
+            });
+    }
+
+
+    private void OnDestroy()
+    {
+        if (fadeTween != null)
+            fadeTween.Kill();
+    }
+}
